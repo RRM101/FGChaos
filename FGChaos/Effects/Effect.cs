041778dabddf6507ce5dfa42afe566999e89714c
RickRollMaster101 <82796006+RickRollMaster101@users.noreply.github.com>
2024-02-16 18:47:52 +0530

    Start of rewrite, add an Effect class.

---
+++ FGChaos/Effects/Effect.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FGChaos.Effects
+{
+    public abstract class Effect
+    {
+        public string Name;
+
+        public int Duration;
+
+        public List<Effect> BlockedEffects;
+
+        public abstract void Run();
+
+        public abstract void End();
+    }
+}
