1d37c0d660ca6103a458c508cafe9691ee75c368
RickRollMaster101 <82796006+RickRollMaster101@users.noreply.github.com>
2024-02-17 18:56:36 +0530

    Added more effects to the new system

---
+++ FGChaos/Effects/LowGravity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace FGChaos.Effects
+{
+    public class LowGravity : Effect
+    {
+        public override string Name
+        {
+            get { return "Low Gravity"; }
+        }
+
+        public override int Duration
+        {
+            get { return 30; }
+        }
+
+        public override void Run()
+        {
+            Physics.gravity = new Vector3(0, -5, 0);
+            base.Run();
+        }
+
+        public override void End()
+        {
+            Physics.gravity = new Vector3(0, -5, 0);
+            base.End();
+        }
+    }
+}
