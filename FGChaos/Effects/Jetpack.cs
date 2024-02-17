3022c78b0fa31558cd7cf7d47716bfc981b5a065
RickRollMaster101 <82796006+RickRollMaster101@users.noreply.github.com>
2024-02-17 18:51:48 +0530

    Added more effects to the new system

---
+++ FGChaos/Effects/Jetpack.cs
@@ -0,0 +1,41 @@
+﻿using Rewired;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FGChaos.Effects
+{
+    public class Jetpack : Effect
+    {
+        public override string Name
+        {
+            get { return "Jetpack"; }
+        }
+
+        public override int Duration
+        {
+            get { return 20; }
+        }
+
+        Player rewiredplayer;
+
+        public override void Run()
+        {
+            rewiredplayer = chaos.fallGuy.GetComponent<FallGuysCharacterControllerInput>()._rewiredPlayer;
+            base.Run();
+        }
+
+        public override void Update()
+        {
+            Chaos.rocketShip = rewiredplayer.GetButton(2);
+        }
+
+        public override void End()
+        {
+            Chaos.rocketShip = false;
+            base.End();
+        }
+    }
+}
---
+++ FGChaos/Effects/RocketShip.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FGChaos.Effects
+{
+    public class RocketShip : Effect
+    {
+        public override string Name
+        {
+            get { return "Rocket Ship"; }
+        }
+
+        public override int Duration
+        {
+            get { return 15; }
+        }
+
+        public override void Run()
+        {
+            Chaos.rocketShip = true;
+            base.Run();
+        }
+
+        public override void End()
+        {
+            Chaos.rocketShip = false;
+            base.End();
+        }
+    }
+}
