ffaf10fb696b8ff6a411beab49985069fb52283b
RickRollMaster101 <82796006+RickRollMaster101@users.noreply.github.com>
2024-05-11 14:10:22 +0530

    Add more music

--- FGChaos/Effects/ChangeMusic.cs
+++ FGChaos/Effects/ChangeMusic.cs
@@ -16,7 +16,7 @@ namespace FGChaos.Effects
             string[] musicNames = music.Keys.ToArray();
             chosenMusic = musicNames[UnityEngine.Random.Range(0, musicNames.Length)];
             Name = $"Change Music to {chosenMusic}";
-            Duration = 20;
+            Duration = 30;
             BlockedEffects = new Type[] { typeof(ChangeMusic) };
         }
 
@@ -27,7 +27,16 @@ namespace FGChaos.Effects
             {"Orbital Tumbling", new string[]{"BNK_Music_Cheap_Ethics", "MUS_InGame_Cheap_Ethics"} },
             {"Satellite Scramble", new string[]{ "BNK_Music_Rung_Equip", "MUS_InGame_Rung_Equip" } },
             {"Temple Tumble", new string[]{ "BNK_Music_Temple_Tumble", "MUS_InGame_Temple_Tumble" } },
-            {"Crown Stack Overflow", new string[]{ "BNK_Music_Swallow_Bottom", "MUS_InGame_Swallow_Bottom" } }
+            {"Crown Stack Overflow", new string[]{ "BNK_Music_Swallow_Bottom", "MUS_InGame_Swallow_Bottom" } },
+            {"Tentacle Tantrum", new string[]{ "BNK_Music_Critic_Carrot", "MUS_InGame_Critic_Carrot" } },
+            {"Scubabeans", new string[]{ "BNK_Music_Bishop_Steak", "MUS_InGame_Bishop_Steak" } },
+            {"Chimney Bean Bebop", new string[]{ "BNK_Music_Chimney_Bean_Bebop", "MUS_InGame_Chimney_Bean_Bebop" } },
+            {"Fall for the Queen Bean", new string[]{ "BNK_Music_Fall_Queen_Bean", "MUS_InGame_Fall_Queen_Bean" } },
+            {"Beans of the Round Table", new string[]{ "BNK_Music_Beans_Round_Table", "MUS_InGame_Beans_Round_Table" } },
+            {"Long Wall", new string[]{ "BNK_Music_Long_Wall", "MUS_InGame_Long_Wall" } },
+            {"Chill Your Beans", new string[]{ "BNK_Music_FallinFriends", "MUS_InGame_FallinFriends" } },
+            {"Falling Stars", new string[]{ "BNK_Music_Falling_Stars", "MUS_InGame_Falling_Stars" } },
+            {"Future Utobeania", new string[]{ "BNK_Music_Future_Utobeania", "MUS_InGame_Future_Utobeania" } }
         };
 
         EventInstance eventInstance;
